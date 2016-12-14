52838199f6079ea1d149fb0207c0c08c395e8508
Priyanshu Goyal <Priyanshu Goyal>
2016-12-14 21:04:08 +0530

    Email login added, signout fixed

--- app/src/main/java/com/pri/android/splitbills/MainActivity.java
+++ app/src/main/java/com/pri/android/splitbills/MainActivity.java
@@ -8,10 +8,13 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -39,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private Button signOutBt;
 
     private ProgressDialog mProgress;
+    private ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         mUsersDatabaseReference = mFirebaseDatabase.getReference().child("Users");
 
         mProgress = new ProgressDialog(this);
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(ProgressBar.INVISIBLE);
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -66,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
                     startActivityForResult(
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
-                                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
                                     .setIsSmartLockEnabled(false)
                                     .build(),
                             RC_SIGN_IN);
@@ -77,9 +84,19 @@ public class MainActivity extends AppCompatActivity {
         signOutBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AuthUI.getInstance().signOut(MainActivity.this);
+                signout();
             }
         });
+    }
+
+    private void signout() {
+        AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "signed out", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
